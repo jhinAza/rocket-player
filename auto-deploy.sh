1ed539ee0d713cbdc62228b01ac3733c6a648155
Julian Diez <juliand1993@gmail.com>
2017-03-29 18:48:13 +0200

    Add a script for automatic deploying

---
+++ auto-deploy.sh
@@ -0,0 +1,19 @@
+if [ $EUID -eq 0 ]; then
+  # User is Root
+  echo "Pulling from origin"
+  git pull origin
+  if [ -z $1 ]; then
+    path="/var/www/html"
+  else
+    path=$1
+  fi
+  echo "Deleting content of" $path
+  rm -rf $path/*
+  echo "Copying www to " $path
+  cp -R www/* $path/
+  echo "Restarting Apache"
+  service apache2 restart
+else
+  echo "User must be root to deploy"
+  exit 1
+fi
