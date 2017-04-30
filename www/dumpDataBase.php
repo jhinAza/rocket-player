0bb9157be6b9a0ace51ff2d37de5ff3e8e5829b6
julian <juliand1993@gmail.com>
2017-04-30 14:42:23 +0200

    Add the endpoint to load a file and restore the DB using it

---
+++ www/dumpDataBase.php
@@ -0,0 +1,33 @@
+<?php
+  require_once("inc/functions.php");
+  require_once("inc/databaseController.php");
+  $db = new DatabaseController();
+  session_start();
+  $user = $_SESSION["user"];
+  session_write_close();
+  if (isUserLoggedIn() && $db->isUserAdmin($user)) {
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+      writeHeader("Dump");
+      writeNavbar();
+      ?>
+      <div class="container" style="padding-top: 76px;">
+        <form action="dumpDataBase.php" method="post" enctype="multipart/form-data">
+          <input type="file" name="schema" id="schema">
+          <input type="submit" name="send" value="Enviar">
+        </form>
+      </div>
+      <?php
+    } else {
+      if ($_FILES["schema"]) {
+        // print($_FILES["schema"]["tmp_name"]);
+        $file = file_get_contents($_FILES["schema"]["tmp_name"]);
+        $file = str_replace("{{dabatase}}", $db->name, $file);
+        $db->executeQuery($file);
+        header("Location: /login.php");
+      }
+    }
+  } else {
+    header("Location: /http404.php");
+
+  }
+ ?>
