4e212826eb15a7dba1f3bce46319732912f9e4db
julian <juliand1993@gmail.com>
2017-04-30 13:11:32 +0200

    Add buttons for reactions and response to comments

--- www/inc/commentsParser.php
+++ www/inc/commentsParser.php
@@ -11,8 +11,26 @@
               <img src="/static/img/user.png"  class="img-prueba">
             </div>
             <div class="col-md-9">
+              <div class="row">
+                <div class="col-md-8">
+                  <h4><?php print($user); ?></h4>
+                </div>
+                <div class="col-md-4">
+                  <div class="btn-group btn-group-sm comments-btn">
+                    <div class="btn btn-info">
+                      <span class="">Responder</span>
+                    </div>
+                    <div class="btn btn-success">
+                      <span class="glyphicon glyphicon-plus"></span>
+                    </div>
+                    <div class="btn btn-danger">
+                      <span class="glyphicon glyphicon-minus"></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <p>
-                <h4><?php print($user); ?></h4>
+
                 <?php print($comment["Comments"]); ?>
               </p>
             </div>
