584131b60a1f13414079d45ff789deb28e14e365
julian <juliand1993@gmail.com>
2017-05-02 14:47:21 +0200

    Add a function to generate a row of history for a specific user

--- www/inc/functions.php
+++ www/inc/functions.php
@@ -103,4 +103,37 @@
     }
     return rmdir($dir);
   }
+
+  function getRowOfHistory($uid, $start=0) {
+    require_once("inc/databaseController.php");
+    $db = new DatabaseController();
+    $row = $db->getHistory($uid, $start, 3);
+    if ($row) {
+      print('<div class="row">');
+      foreach ($row as $video) {
+        ?>
+        <div class="col-md-4 item">
+          <div class="row">
+            <div class="col-md-6 video-data">
+              <div class="row">
+                <h4>
+                  <a href=<?php print("/player?video=".$video["videoID"]) ?>>
+                    <?php print($video["videoname"]) ?>
+                  </a>
+                </h4>
+              </div>
+              <div class="row">
+                <h5><?php print($video["creator"]) ?></h5>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <img src="/static/img/video.jpg" alt="" style="width:100%">
+            </div>
+          </div>
+        </div>
+        <?php
+      }
+      print("</div>");
+    }
+  }
 ?>
