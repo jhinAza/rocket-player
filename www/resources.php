0a1d399b733b5d05c3d48155aa4110da0a1c3033
julian <juliand1993@gmail.com>
2017-05-07 18:50:46 +0200

    Add backend and frontend code to check if the form is valid and store the data in the DB

--- www/resources.php
+++ www/resources.php
@@ -14,7 +14,7 @@ require_once("inc/functions.php");
       <!-- Now we return a form -->
       <div class="container" style="padding-top:76px" id="main">
         <!-- <?php print_r($video); ?> -->
-        <form id="upload" method="multipart/form-data">
+        <form id="upload-res" method="multipart/form-data">
           <fieldset>
             <legend>Upload resources form</legend>
             <div class="well">
@@ -50,7 +50,7 @@ require_once("inc/functions.php");
                     <span class="input-group-btn">
                       <label class="file-label btn btn-default">
                         Selecciona el fichero
-                        <input type="file" name="video-input" id="video-input" accept="video/mp4">
+                        <input type="file" name="res-input" id="res-input">
                       </label>
                     </span>
                   </div>
@@ -59,11 +59,10 @@ require_once("inc/functions.php");
               <div class="col-md-6">
                 <label for="video-category">Selecciona un tipo de recurso</label>
                 <div class="form-group">
-                  <select class="form-control" name="resource-lang" id="resource-lang">
-                    <option value="1">Subtitulos</option>
-                    <option value="2">Transcripcion</option>
-                    <option value="2">Lenguaje de señas</option>
-                    <option value="2">Pista de audio</option>
+                  <select class="form-control" name="resource-type" id="resource-type">
+                    <option value="subtitles">Subtitulos</option>
+                    <option value="transcription">Transcripcion</option>
+                    <option value="signal-language">Lenguaje de señas</option>
                   </select>
                 </div>
               </div>
@@ -73,14 +72,14 @@ require_once("inc/functions.php");
                 <label for="video-category">Selecciona un lenguaje</label>
                 <div class="form-group">
                   <select class="form-control" name="resource-lang" id="resource-lang">
-                    <option value="1">Ingles</option>
-                    <option value="2">Español</option>
+                    <option value="english">Ingles</option>
+                    <option value="spanish">Español</option>
                   </select>
                 </div>
               </div>
               <div class="col-md-6" style="padding-top:25px">
                 <div class="btn-group col-md-8 col-md-push-2">
-                  <input type="submit" name="send-form" id="send-form" value="Enviar" class="btn btn-success col-md-6">
+                  <input type="submit" name="send-res-form" id="send-res-form" value="Enviar" class="btn btn-success col-md-6">
                   <input type="reset" name="reset" id="reset" value="Borrar" class="btn btn-danger col-md-6">
                 </div>
               </div>
@@ -98,12 +97,33 @@ require_once("inc/functions.php");
     } elseif (isUserLoggedIn()) {
       // debug(__FILE__,__FUNCTION__,__LINE__);
       // If the user is logged then will start the upload
-      $uploadDirectory = "res/video";
+      $res_type = $_POST["resource-type"];
+      switch ($res_type) {
+        case 'subtitles':
+          $uploadDirectory = "res/subs";
+          $file_tipe = '/.srt$/';
+          break;
+        case 'transcription':
+          $uploadDirectory = "res/trans";
+          $file_tipe = '/.srt$/';
+          break;
+        case 'signal-language':
+          $uploadDirectory = "res/signal";
+          $file_tipe = '/.mp4$/';
+          break;
+        default:
+          http_response_code(500);
+          die();
+          break;
+      }
       if (!file_exists($uploadDirectory)) {
         mkdir($uploadDirectory, 0777, true);
       }
-      switch (strtolower($_FILES["video-input"]["type"])) {
-        case 'video/mp4':
+      $name = strtolower($_FILES["res-input"]["name"]);
+      error_log($name);
+      switch ($name) {
+        case preg_match($file_tipe, $name) ? true : false:
+          error_log("Yep!!");
           break;
         default:
           error_log("El fichero no es correcto");
@@ -111,21 +131,20 @@ require_once("inc/functions.php");
           break;
       }
       // We save the data needed for the rename
-      $hash_file = hash_file("sha256", $_FILES["video-input"]["tmp_name"]);
+      $hash_file = hash_file("sha256", $_FILES["res-input"]["tmp_name"]);
       $timestamp = time();
-      $file_name = strtolower($_FILES["video-input"]["name"]);
+      $file_name = strtolower($_FILES["res-input"]["name"]);
       $ext = substr($file_name, strrpos($file_name, "."));
       $new_name = "$hash_file-$timestamp$ext";
       $full_name = $uploadDirectory."/".$new_name;
-      move_uploaded_file($_FILES["video-input"]["tmp_name"], $full_name);
+      move_uploaded_file($_FILES["res-input"]["tmp_name"], $full_name);
       // Now we only need save the data in the database
-      $videoname = $_POST["video-name"];
-      $tags = $_POST["video-tags"];
-      $desc = $_POST["video-desc"];
-      $cat = $_POST["video-category"];
+      $lang = $_POST["resource-lang"];
+      $type = $_POST["resource-type"];
+      $video = $_POST["video"];
       require_once("inc/databaseController.php");
       $db = new DatabaseController();
-      $db->saveVideoInfo($videoname, $tags, $desc, $cat, $new_name, $timestamp);
+      $db->saveResourceInfo($new_name, $type, $lang, $video);
     } else {
       // If not, we will return him to the login page
       header("Location: login.php");
