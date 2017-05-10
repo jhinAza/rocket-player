53591b9749298ba8b746ea64459c2ed94df79a32
julian <juliand1993@gmail.com>
2017-05-10 22:41:36 +0200

    Update function to get user preferred srt file for asking the preferred lang

--- www/inc/functions.php
+++ www/inc/functions.php
@@ -173,11 +173,14 @@
   }
 
   function getUserPreferredSubtitlesFile ($user, $video) {
+    require_once("inc/userSettingsReader.php");
+    $reader = new UserSettingsReader($user);
     require_once("inc/databaseController.php");
     $db = new DatabaseController();
-    $uid = $db->getUserID($user);
-    $file = $db->getSubtitlesFile($video);
-    error_log($file);
-    return "res/subs/".$file;
+    $file = $db->getSubtitlesFile($video, $reader->getLang("1"));
+    if ($file) {
+      return "res/subs/".$file;
+    }
+    return false;
   }
 ?>
