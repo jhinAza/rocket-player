b81c09fa8d72fbee04adf051b66a7fa658b8a92b
julian <juliand1993@gmail.com>
2017-05-14 12:26:15 +0200

    Add endpoint for searching by query users or videos

---
+++ www/search.php
@@ -0,0 +1,86 @@
+<!-- This file wil include everything needed for the home page -->
+<?php
+  require_once("inc/functions.php");
+  require_once("inc/databaseController.php");
+  if (isUserLoggedIn()) {
+    writeHeader("home");
+    writeNavbar();
+    $db = new DatabaseController();
+    ?>
+      <div class="container" style="padding-top:76px;">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="panel panel-info">
+              <div class="panel-heading">
+                <h4 class="panel-title">Videos</h4>
+              </div>
+              <div class="panel-body">
+                <?php
+                $list = ($db->getVideosByQuery($_GET["query"]));
+                foreach ($list as $item) {
+                  ?>
+                  <div class="col-md-6 item">
+                    <div class="row">
+                      <div class="col-md-6">
+                        <div class="row">
+                          <h4>
+                            <a href=<?php print("/player?video=".$item["id"]) ?>>
+                              <?php print($item["videoname"]) ?>
+                            </a>
+                          </h4>
+                        </div>
+                        <div class="row">
+                          <h5><?php print($db->getUserName($item["userid"])) ?></h5>
+                        </div>
+                      </div>
+                      <div class="col-md-6">
+                        <img src="/static/img/video.jpg" alt="" style="width:100%">
+                      </div>
+                    </div>
+                  </div>
+                  <?php
+                }
+                ?>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="panel panel-info">
+              <div class="panel-heading">
+                <h4 class="panel-title">Usuarios</h4>
+              </div>
+              <div class="panel-body">
+                <?php
+                $list = ($db->getUsersByQuery($_GET["query"]));
+                foreach ($list as $item) {
+                  ?>
+                  <div class="col-md-6 item user">
+                    <div class="row">
+                      <div class="col-md-6">
+                        <div class="row">
+                          <h4>
+                            <a href=<?php print("/profile?uid=".$item["id"]) ?>>
+                              <?php print($item["username"]) ?>
+                            </a>
+                          </h4>
+                        </div>
+                      </div>
+                      <div class="col-md-6">
+                        <img src="/static/img/user.jpg" alt="" style="width:100%">
+                      </div>
+                    </div>
+                  </div>
+                  <?php
+                }
+                ?>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    <?php
+  } else {
+    header("Location:/login.php");
+  }
+  writeFooter();
+?>
