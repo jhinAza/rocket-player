c13a62ed736dadac79605fbb65610f84e18236da
julian <juliand1993@gmail.com>
2017-05-15 12:44:02 +0200

    Try to fix bug

--- auto-deploy.sh
+++ auto-deploy.sh
@@ -8,7 +8,11 @@ if [ $EUID -eq 0 ]; then
     path=$1
   fi
   echo "Deleting content of" $path
+  mv $path/res /tmp/res
+  mv $path/userSettings /tmp/userSettings
   rm -rf $path/!(res|userSettings)
+  mv /tmp/res $path/res
+  mv /tmp/userSettings $path/userSettings
   echo "Copying www to " $path
   cp -R www/* $path/
   echo "Changing the permission of" $path
