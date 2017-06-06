ba7a957faa5ec3dc084bd570f972bbb01896f0ee
julian <juliand1993@gmail.com>
2017-06-06 23:50:36 +0200

    Trying to fix problem

--- auto-deploy.sh
+++ auto-deploy.sh
@@ -10,13 +10,13 @@ if [ $EUID -eq 0 ]; then
   echo "Deleting content of" $path
   mv $path/res /tmp/res
   rm -rf /tmp/userSettings
-  rm -rf /tmp/res
   mv $path/userSettings /tmp/userSettings
-  rm -rf $path/
-  mv /tmp/res $path/
   mv /tmp/userSettings $path/
+  rm -rf $path/
   echo "Copying www to " $path
   cp -R www/ $path/
+  mv /tmp/res $path/
+  rm -rf /tmp/res
   echo "Changing the permission of" $path
   chmod -R a+w $path/*
   echo "Restarting Apache"
